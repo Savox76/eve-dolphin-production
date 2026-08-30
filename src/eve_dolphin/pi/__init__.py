1273953ca3b08b90e7ea8b3a93837cd0ceb387d9
Savoxmedia <135124987+Savox76@users.noreply.github.com>
2026-08-30 22:31:47 +0200

    feat: add planetary colony overview

---
+++ src/eve_dolphin/pi/__init__.py
@@ -0,0 +1,15 @@
+"""Planetary-industry read models and planning services."""
+
+from eve_dolphin.pi.overview import (
+    ColonyOverview,
+    NamedCount,
+    NamedQuantity,
+    PlanetaryOverviewService,
+)
+
+__all__ = [
+    "ColonyOverview",
+    "NamedCount",
+    "NamedQuantity",
+    "PlanetaryOverviewService",
+]
